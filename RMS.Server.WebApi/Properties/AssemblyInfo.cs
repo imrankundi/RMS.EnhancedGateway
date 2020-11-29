50bf0c9b8f98975a1acdbe628ff3ef99d32007fb
Imranullah Khan <kundi.imranullah@gmail.com>
2020-11-29 19:02:30 +0500

    Driver made for Reon According to there requirements

--- RMS.Server.WebApi/Properties/AssemblyInfo.cs
+++ RMS.Server.WebApi/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RMS.Server.WebApi")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RMS Enhanced Gateway")]
+[assembly: AssemblyDescription("RMS Enhanced Gateway")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("SalTec Powerlink")]
 [assembly: AssemblyProduct("RMS")]
 [assembly: AssemblyCopyright("Copyright © SalTec Powerlink 2020")]
 [assembly: AssemblyTrademark("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.6")]
+[assembly: AssemblyFileVersion("1.0.0.6")]
